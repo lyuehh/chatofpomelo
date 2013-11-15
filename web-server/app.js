300f34e55e7b1f7e9aab4b6c81879681ef49bec8
lyuehh <lyuehh@gmail.com>
2013-11-15 15:15:27 +0800

    update express to 3.0

--- web-server/app.js
+++ web-server/app.js
@@ -1,26 +1,27 @@
+var http = require('http');
+var path = require('path');
 var express = require('express');
-var app = express.createServer();
+var app = express();
+var server = http.createServer(app);
 
-app.configure(function(){
-	app.use(express.methodOverride());
-	app.use(express.bodyParser());
-	app.use(app.router);
-	app.set('view engine', 'jade');
-	app.set('views', __dirname + '/public');
-	app.set('view options', {layout: false});
-	app.set('basepath',__dirname + '/public');
+// all environments
+app.set('port', process.env.PORT || 3001);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+server.listen(app.get('port'), function() {
+    console.log('server running on %s', app.get('port'));
 });
-
-app.configure('development', function(){
-	app.use(express.static(__dirname + '/public'));
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-	var oneYear = 31557600000;
-	app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-	app.use(express.errorHandler());
-});
-
-console.log("Web server has started.\nPlease log on http://127.0.0.1:3001/index.html");
-app.listen(3001);
